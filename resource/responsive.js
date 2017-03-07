c5e75aadce05532950178b6b2fa97d395c3f2d8a
ZhaoC <edwardcai1993@gmail.com>
2017-03-06 21:50:10 -0500

    migrate to hexo

--- resource/responsive.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-$(document).ready(function() {
-  $('.mobile-nav').click(function() {
-    $('.main-container').toggleClass('menu-active');
-    $('.mobile-nav').toggleClass('menu-active');
-    $('.side-nav').toggleClass('menu-active');
-  });
-});
