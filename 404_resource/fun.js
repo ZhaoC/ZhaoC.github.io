4fdf98315a54b021fbd0dba40d86f645ab01aede
ZhaoC <edwardcai1993@gmail.com>
2017-11-04 23:37:01 -0400

    update flash speed

--- 404_resource/fun.js
+++ 404_resource/fun.js
@@ -7,17 +7,5 @@ var animDataOne = {
     path: '404_resource/fun_animation.json'
 };
 
-var animDataTwo = {
-    wrapper: document.getElementById('svgContainerTwo'),
-    animType: 'svg',
-    loop: true,
-    prerender: true,
-    autoplay: true,
-    path: '404_resource/fun_animation.json'
-};
-
 var animOne = bodymovin.loadAnimation(animDataOne);
-animOne.setSpeed(1.2);
-
-var animTwo = bodymovin.loadAnimation(animDataTwo);
-animTwo.setSpeed(3);
+animOne.setSpeed(2.5);
