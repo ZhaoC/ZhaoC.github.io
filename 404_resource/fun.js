524d678cce1e9dede7dcfc6a26bbaf05c1e9a9fe
ZhaoC <edwardcai1993@gmail.com>
2017-11-04 23:29:06 -0400

    update 404 page and add fun pages

---
+++ 404_resource/fun.js
@@ -0,0 +1,23 @@
+var animDataOne = {
+    wrapper: document.getElementById('svgContainerOne'),
+    animType: 'svg',
+    loop: true,
+    prerender: true,
+    autoplay: true,
+    path: '404_resource/fun_animation.json'
+};
+
+var animDataTwo = {
+    wrapper: document.getElementById('svgContainerTwo'),
+    animType: 'svg',
+    loop: true,
+    prerender: true,
+    autoplay: true,
+    path: '404_resource/fun_animation.json'
+};
+
+var animOne = bodymovin.loadAnimation(animDataOne);
+animOne.setSpeed(1.2);
+
+var animTwo = bodymovin.loadAnimation(animDataTwo);
+animTwo.setSpeed(1.3);
